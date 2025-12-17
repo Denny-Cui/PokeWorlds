0814dfce61f503b9f00da7642fbf8ee58ecd08f5
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2025-12-17 10:21:31 -0800

    Testing docs

--- src/poke_env/__init__.py
+++ src/poke_env/__init__.py
@@ -2,9 +2,10 @@ from poke_env.emulators.emulator import Emulator, GameStateParser, LowLevelActio
 from poke_env.emulators.pokemon import get_pokemon_emulator, VARIANT_TO_GB_NAME
 from poke_env.emulators.pokemon.parsers import PokemonGameStateParser, BasePokemonRedGameStateParser, MemoryBasedPokemonRedGameStateParser
 from poke_env.emulators.pokemon.controllers import PokemonController
+from poke_env.emulators.pokemon import parsers
 
 AVAILABLE_POKEMON_VARIANTS = list(VARIANT_TO_GB_NAME.keys())
 """ List of available Pokemon game variants. """
 
-__all__ = [LowLevelActions, AVAILABLE_POKEMON_VARIANTS, get_pokemon_emulator, Emulator, GameStateParser, PokemonGameStateParser, BasePokemonRedGameStateParser, MemoryBasedPokemonRedGameStateParser, PokemonController]
+__all__ = [LowLevelActions, AVAILABLE_POKEMON_VARIANTS, get_pokemon_emulator, Emulator, parsers, GameStateParser, PokemonGameStateParser, BasePokemonRedGameStateParser, MemoryBasedPokemonRedGameStateParser, PokemonController]
 #__all__ = ["LowLevelActions", "AVAILABLE_POKEMON_VARIANTS", "get_pokemon_emulator", "Emulator", "GameStateParser", "PokemonGameStateParser", "BasePokemonRedGameStateParser", "MemoryBasedPokemonRedGameStateParser"]
