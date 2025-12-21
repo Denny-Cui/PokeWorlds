74470d57f9b4f55bbd9e007cbcf78f2f3f96d32a
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2025-12-20 16:42:12 -0800

    Try

--- demos/charmander_enthusiast.py
+++ demos/charmander_enthusiast.py
@@ -4,6 +4,7 @@ from gymnasium.spaces import Discrete, OneOf
 import numpy as np
 
 num_cpu = 4  # Number of processes to use
+render = False # Whether to render the environment at test time
 
 
 class OneOfToDiscreteWrapper(gym.ActionWrapper):
@@ -47,33 +48,35 @@ from stable_baselines3 import DQN, PPO
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.vec_env import SubprocVecEnv
 
-env = SubprocVecEnv([make_env(i) for i in range(num_cpu)])
+if __name__ == "__main__":
+    env = SubprocVecEnv([make_env(i) for i in range(num_cpu)])
 
-# Instantiate the agent
-model = PPO("MultiInputPolicy", env, verbose=1, gamma=0.999, )
-# Train the agent and display a progress bar
-model.learn(total_timesteps=int(2e5), progress_bar=True)
-# Save the agent
-model.save("charmander_enthusiast_agent")
-del model  # delete trained model to demonstrate loading
+    # Instantiate the agent
+    model = PPO("MultiInputPolicy", env, verbose=1, gamma=0.999, )
+    # Train the agent and display a progress bar
+    model.learn(total_timesteps=int(2e5), progress_bar=True)
+    # Save the agent
+    model.save("charmander_enthusiast_agent")
+    del model  # delete trained model to demonstrate loading
 
-# Load the trained agent
-# NOTE: if you have loading issue, you can pass `print_system_info=True`
-# to compare the system on which the model was trained vs the current one
-# model = DQN.load("dqn_lunar", env=env, print_system_info=True)
-model = PPO.load("charmander_enthusiast_agent", env=env)
+    # Load the trained agent
+    # NOTE: if you have loading issue, you can pass `print_system_info=True`
+    # to compare the system on which the model was trained vs the current one
+    # model = DQN.load("dqn_lunar", env=env, print_system_info=True)
+    model = PPO.load("charmander_enthusiast_agent", env=env)
 
-# Evaluate the agent
-# NOTE: If you use wrappers with your environment that modify rewards,
-#       this will be reflected here. To evaluate with original rewards,
-#       wrap environment in a "Monitor" wrapper before other wrappers.
-mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
-print(f"Reward: {mean_reward} +/- {std_reward}")
+    # Evaluate the agent
+    # NOTE: If you use wrappers with your environment that modify rewards,
+    #       this will be reflected here. To evaluate with original rewards,
+    #       wrap environment in a "Monitor" wrapper before other wrappers.
+    mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
+    print(f"Reward: {mean_reward} +/- {std_reward}")
 
-# Enjoy trained agent
-vec_env = model.get_env()
-obs = vec_env.reset()
-for i in range(1000):
-    action, _states = model.predict(obs, deterministic=True)
-    obs, rewards, dones, info = vec_env.step(action)
-    vec_env.render()
+    # Enjoy trained agent
+    if render:
+        vec_env = model.get_env()
+        obs = vec_env.reset()
+        for i in range(1000):
+            action, _states = model.predict(obs, deterministic=True)
+            obs, rewards, dones, info = vec_env.step(action)
+            vec_env.render()
