49faa4f429fe90a28da06547683ef3ff59cc6fbc
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2026-01-09 01:00:55 -0800

    update docs

--- src/poke_worlds/emulation/__init__.py
+++ src/poke_worlds/emulation/__init__.py
@@ -1,13 +1,13 @@
 """
-The classes here define the most high level abstractions of the fundamental classes used for emulation. 
-1. `Emulator`: The core class that handles the emulation of Pokémon games.
-2. `StateParser`: Responsible for parsing the game state from the emulator when called. Is called by a `StateTracker`.
-3. `StateTracker`: Keeps track of the game state over time, allowing for state comparisons and history tracking. Is called once per step in the `Emulator`. 
+This submodule handles the emulation of Gameboy games. The core classes here are:
+1. `StateParser`: Responsible for parsing the game state from the screen, and the memory states of the gameboy. Can be used to extract relevant information from the game state at each step (e.g. player location, current party, inventory items, etc).
+2. `StateTracker`: Uses the `StateParser` and pre-registers specific state tracking logic that is called at after every step in the game. This class is primarily responsible for maintaining the game state information at each step, which is represented by a nested dictionary of string keys and subkeys. It also tracks relevant metrics over playthroughs (e.g. number of battles won) and aggregates metrics over multiple playthroughs (across resets) if required. 
+3. `Emulator`: The core class that handles the emulation of the game. It carries a `StateParser` and `StateTracker`. Every step runs a single button press, and ensures that the state tracker is properly updated.
 
 Briefly skim the documentation for each of these classes to understand their roles, the fundamental structure they impose and how they interact with each other.
 
 In practice, unless you are implementing new games, you will not need to interact with these base classes directly. Each have subclasses that implement Pokémon specific logic and provides some additional structure.
-This is what you should familiarize yourself with most deeply if you wish to use this package as a black box API and not care about the internals. Go to the `pokemon` submodule and look at the parsers and trackers defined there. 
+This is what you should familiarize yourself with most deeply if you wish to use this package as a black box API and not care about the internals. Go to the `pokemon` submodule and look at the classes defined there. 
 """
 from poke_worlds.emulation.emulator import Emulator, LowLevelActions
 from poke_worlds.emulation.tracker import StateTracker
---
+++ src/poke_worlds/execution/__init__.py
@@ -0,0 +1,3 @@
+"""
+This submodule provides access to the `Executor`, a VLM agent that can use high level actions to try and achieve a simple, but arbitrary, natural language task in a given game environment.
+"""
